b7fb524e3e92cee2c31daed1f7a36b754648e481
日崎智博 <magatama.club@gmail.com>
2019-04-04 02:28:15 +0900

    TypeScript 導入

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 // next.config.js
+const withTypescript = require('@zeit/next-typescript')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-    sassLoaderOptions: {
-        url: false
-    }
-})
+
+module.exports = withTypescript(
+    withSass({
+        sassLoaderOptions: {
+            url: false
+        },
+        webpack( config, options ) {
+            // Do not run type checking twice:
+            if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
+            
+            return config
+        }
+    })
+)
