f6e692830624632db2604eebcde21f2b3d51ea4a
日崎智博 <magatama.club@gmail.com>
2019-04-14 18:25:31 +0900

    静的コーディング1

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,55 +2,57 @@ import Link from 'next/link'
 import css from './style.scss'
 
 const Header = () => (
-  <header id="header">
-    <h1 className="">
-      <Link href="/">
-        <a>パフォーミングアーティスト 徳島はっちー</a>
-      </Link>
-    </h1>
-    <ul className={`${css['f-flex']} ${css['f-flex_mg30']}`}>
-      <li className={css['f-flex3']}>
-        <Link href="">
-          <a>トップ</a>
+  <header id="header" className={css.header}>
+    <div className={css.inner}>
+      <h1 className={css.title}>
+        <Link href="/">
+          <a><img src="/static/images/title.svg" alt="" /></a>
         </Link>
-      </li>
-      <li className={css['f-flex3']}>
+      </h1>
+      <ul className={`${css.list} ${css['f-flex']} ${css['f-flex_mg30']}`}>
+        <li className={css['f-flex3']}>
+          <Link href="">
+            <a>トップ</a>
+          </Link>
+        </li>
+        <li className={css['f-flex3']}>
+          <Link href="">
+            <a>お知らせ</a>
+          </Link>
+        </li>
+        <li className={css['f-flex3']}>
+          <Link href="">
+            <a>プロフィール</a>
+          </Link>
+        </li>
+        <li className={css['f-flex3']}>
+          <Link href="">
+            <a>パフォーマンス</a>
+          </Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="">
+            <a>Facebook</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="">
+            <a>Instagram</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="">
+            <a>twitter</a>
+          </Link>
+        </li>
+      </ul>
+      <div>
         <Link href="">
-          <a>お知らせ</a>
+          <a>出演依頼<br />お問合せ</a>
         </Link>
-      </li>
-      <li className={css['f-flex3']}>
-        <Link href="">
-          <a>プロフィール</a>
-        </Link>
-      </li>
-      <li className={css['f-flex3']}>
-        <Link href="">
-          <a>パフォーマンス</a>
-        </Link>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <Link href="">
-          <a>Facebook</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="">
-          <a>Instagram</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="">
-          <a>twitter</a>
-        </Link>
-      </li>
-    </ul>
-    <div>
-      <Link href="">
-        <a>出演依頼<br />お問合せ</a>
-      </Link>
+      </div>
     </div>
   </header>
 )
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,16 +1,36 @@
 import Header from './../Header/index'
 import css from './style.scss'
 
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const Layout = props => (
-  <body className={css['fw']}>
-    <div className={css['container']}>
-      <Header />
-      <main id="contents" className={css['f-max']}>
-      {props.children}
-      </main>
-    </div>
-  </body>
-)
+  componentDidMount() {
+    // console.log('Yeah!!')
+
+    (function(d) {
+      var config = {
+        kitId: 'chb4lrd',
+        scriptTimeout: 3000,
+        async: true
+      },
+      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
+  }
+
+  render() {
+    return (
+      <div className={css['fw']}>
+        <div className={css['container']}>
+          <Header />
+          <main id="contents" className={css['f-max']}>
+          {this.props.children}
+          </main>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Layout
